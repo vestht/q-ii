ade26b9c79aa54a890841ba900767e21f351161b
Shyamin Ayesh <me@shyamin.com>
2024-05-09 16:25:27 +0530

    define compute module

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 module "networking" {
     source = "./modules/networking"
 }
+
+module "compute" {
+    source = "./modules/compute"
+    instance_1a_subnet_id = module.networking.instance_1a_subnet_id
+    instance_1a_security_group_id = module.networking.instance_1a_security_group_id
+}
