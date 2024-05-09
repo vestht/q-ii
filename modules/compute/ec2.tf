3cade66ee440b40a71ed004725d46de018cd2b7f
Shyamin Ayesh <me@shyamin.com>
2024-05-09 16:40:37 +0530

    define key pair

--- modules/compute/ec2.tf
+++ modules/compute/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "instance" {
     ami = "ami-084cab24460184bd3" # Ubuntu 22.04 (AMD64)
     instance_type = "t3.micro"
+    key_name = aws_key_pair.instance_key_pair.key_name
 
     network_interface {
       network_interface_id = var.instance_1a_nic_0_id
