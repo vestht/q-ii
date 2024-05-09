d9249dab0c3f6b84310a3f1159bd76eec198c3c0
Shyamin Ayesh <me@shyamin.com>
2024-05-09 16:37:28 +0530

    remove unwanted sg id

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -2,8 +2,3 @@
 output "instance_1a_nic_0_id" {
 	value = aws_network_interface.instance_1a_nic_0.id
 }
-
-# Output: instance_1a_security_group_id
-output "instance_1a_security_group_id" {
-	value = aws_security_group.instance_security_group.id
-}
