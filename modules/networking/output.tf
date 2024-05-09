55390b406fc9c95fe6163835fc4aa8dcb76d161a
Shyamin Ayesh <me@shyamin.com>
2024-05-09 16:33:09 +0530

    output nic id

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -1,6 +1,6 @@
-# Output: instance_1a_subnet_id
-output "instance_1a_subnet_id" {
-	value = aws_subnet.instance_subnet_1a.id
+# Output: instance_1a_nic_0_id
+output "instance_1a_nic_0_id" {
+	value = aws_network_interface.instance_1a_nic_0.id
 }
 
 # Output: instance_1a_security_group_id
