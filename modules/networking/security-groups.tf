2bdc8b4c865d65388d9fe583663bb6a5c0f3eca2
Shyamin Ayesh <me@shyamin.com>
2024-05-09 16:25:45 +0530

    security group for instance

---
+++ modules/networking/security-groups.tf
@@ -0,0 +1,24 @@
+# Resource: instance_security_group
+resource "aws_security_group" "instance_security_group" {
+	vpc_id = aws_vpc.vpc.id
+
+	ingress {
+		to_port = 22
+		from_port = 22
+		protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+		description = "ANY:SSH"
+	}
+
+	egress {
+		to_port = 0
+		from_port = 0
+		protocol = -1
+		cidr_blocks = [ "0.0.0.0/0" ]
+		description = "ANY:ANY"
+	}
+
+	tags = {
+		Name = "VESTHT-INSTANCE-1A-SG"
+	}
+}
