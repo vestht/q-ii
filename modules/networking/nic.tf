37f641e51900fabae97d3d900c431a1065ec1c28
Shyamin Ayesh <me@shyamin.com>
2024-05-09 16:34:14 +0530

    remove subnet

--- modules/networking/nic.tf
+++ modules/networking/nic.tf
@@ -1,7 +1,7 @@
 # Resource: instance_1a_nic_0
 resource "aws_network_interface" "instance_1a_nic_0" {
   subnet_id   = aws_subnet.instance_subnet_1a.id
-  private_ips = ["172.16.10.10/32"]
+  private_ips = ["172.16.10.10"]
 
   tags = {
     Name = "VESTHT-INSTANCE-1A-NIC-0"
