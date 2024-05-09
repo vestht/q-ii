c943a34d60bb76d2b2a949b44526e40070e66250
Shyamin Ayesh <me@shyamin.com>
2024-05-09 16:24:21 +0530

    vars

---
+++ modules/compute/variables.tf
@@ -0,0 +1,11 @@
+# Variable: instance_1a_subnet_id
+variable "instance_1a_subnet_id" {
+	type = string
+	nullable = false
+}
+
+# Variable: instance_1a_security_group_id
+variable "instance_1a_security_group_id" {
+	type = string
+	nullable = false
+}
