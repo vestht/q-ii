200ecf5231b8873821930981d6416039fb6f7b90
Shyamin Ayesh <me@shyamin.com>
2024-05-09 16:25:37 +0530

    output ids of sg and subnet

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -1,4 +1,9 @@
-# Output: instance_subnet_1a_id
-output "instance_subnet_1a_id" {
+# Output: instance_1a_subnet_id
+output "instance_1a_subnet_id" {
 	value = aws_subnet.instance_subnet_1a.id
+}
+
+# Output: instance_1a_security_group_id
+output "instance_1a_security_group_id" {
+	value = aws_subnet.instance_1a_security_group_id.id
 }
